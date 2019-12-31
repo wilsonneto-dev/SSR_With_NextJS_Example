76778d55aba0da0a04cff43764c5d9aaf5681a45
Wilson Gomes <contato@wilsonneto.com.br>
2019-12-31 18:26:05 -0300

    ssr and static

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
 // next.config.js
 const withSass = require("@zeit/next-sass");
+const api = require("axios");
+
 module.exports = withSass({
   /* config options here */
+
+  exportTrailingSlash: true,
+  exportPathMap: async () => {
+    const paths = {
+      "/": { page: "/" },
+      "/about": { page: "/about" }
+    };
+
+    const { data } = await api.get(
+      "https://api.tvmaze.com/search/shows?q=batman"
+    );
+    const shows = data.map(i => i.show);
+
+    shows.forEach(show => {
+      paths[`/batman/${show.id}`] = {
+        page: "/batman/[id]",
+        query: { id: show.id }
+      };
+    });
+
+    return paths;
+  }
 });
